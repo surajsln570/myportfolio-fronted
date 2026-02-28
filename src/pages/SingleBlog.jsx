5aeb68bd45bb6de497b1661af347d1a3875bc709
surajsln570 <surajsln570@gmail.com>
2026-02-28 20:22:20 +0530

    a

--- src/pages/SingleBlog.jsx
+++ src/pages/SingleBlog.jsx
@@ -16,17 +16,13 @@ export default function SingleBlog() {
   }, [slug]);
 
   if (!blog) {
-    return <Col>
-      <Navigation />
-      <div className="text-center py-20">Loading...</div>
-    </Col>;
+    return <Col><Navigation /> <div className="text-center py-20">Loading...</div></Col>
   }
 
   return (
     <Col>
       <Navigation />
       <div className="max-w-4xl mx-auto px-6 py-20">
-
         {blog.coverImage && (
           <img
             src={blog.coverImage}
@@ -34,17 +30,14 @@ export default function SingleBlog() {
             className="w-full h-80 object-cover rounded-2xl mb-8"
           />
         )}
-
         <h1 className="text-4xl font-bold mb-4">
           {blog.title}
         </h1>
-
         <div className="text-gray-500 mb-8 text-sm flex gap-4">
           <span>{new Date(blog.createdAt).toDateString()}</span>
           <span>{blog.readingTime}</span>
           <span>{blog.views} views</span>
         </div>
-
         <div
           className="prose max-w-none"
           dangerouslySetInnerHTML={{ __html: blog.content }}
