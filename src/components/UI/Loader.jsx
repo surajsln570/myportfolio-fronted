c9d4431ed129d9da4105e7e3d8fd73a930b7f702
surajsln570 <surajsln570@gmail.com>
2026-03-01 15:14:49 +0530

    a

--- src/components/UI/Loader.jsx
+++ src/components/UI/Loader.jsx
@@ -1,6 +1,6 @@
 export default function Loader() {
   return (
-    <div className="inset-0 flex items-center justify-center bg-black/70 z-50">
+    <div className="fixed inset-0 flex items-center justify-center bg-black/70 z-50">
       <div className="w-12 h-12 border-4 border-gray-500 border-t-cyan-400 rounded-full animate-spin"></div>
     </div>
   );
